6ebc652bcfd69c19e137200f29e52f8a34d8847b
ProSkyMishka <145161036+ProSkyMishka@users.noreply.github.com>
2024-03-26 18:05:15 +0300

    Delete ClassicsRoutes directory

--- ClassicsRoutes/Sources/App/routes.swift
+++
@@ -1,9 +0,0 @@
-import Fluent
-import Vapor
-
-func routes(_ app: Application) throws {
-    try app.register(collection: RoutesController())
-    try app.register(collection: UsersController())
-    try app.register(collection: ChatsController())
-    try app.register(collection: MessagesController())
-}
